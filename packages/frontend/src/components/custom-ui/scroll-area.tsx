d93868cff2064297273721ead3323ea61cce1909
fdemb <69842744+fdemb@users.noreply.github.com>
2026-02-08 20:34:05 +0100

    use rigid-ui for scroll-areas

--- packages/frontend/src/components/custom-ui/scroll-area.tsx
+++ packages/frontend/src/components/custom-ui/scroll-area.tsx
@@ -1,7 +1,6 @@
 import { cn } from "@/lib/utils";
-import * as scrollArea from "@zag-js/scroll-area";
-import { normalizeProps, useMachine } from "@zag-js/solid";
-import { createMemo, createUniqueId, Show, splitProps, type JSX } from "solid-js";
+import { splitProps, type JSX } from "solid-js";
+import { ScrollArea as ScrollAreaPrimitive } from "rigid-ui/scroll-area";
 
 export function ScrollContainer(props: {
   children?: JSX.Element;
@@ -22,76 +21,17 @@ type ScrollAreaProps = {
   class?: string;
   rootClass?: string;
   viewportClass?: string;
-  contentClass?: string;
 };
 
 export function ScrollArea(props: ScrollAreaProps) {
-  const service = useMachine(scrollArea.machine, {
-    id: createUniqueId(),
-  });
-
-  const api = createMemo(() => scrollArea.connect(service, normalizeProps));
-
   return (
-    <div
-      {...api().getRootProps()}
-      class={cn(
-        "flex flex-col overflow-hidden max-h-full group/scrollarea",
-        props.rootClass,
-        props.class,
-      )}
-    >
-      <div
-        {...api().getViewportProps()}
-        class={cn("w-full h-full [scrollbar-width:none] flex-col flex", props.viewportClass)}
-      >
-        <div {...api().getContentProps()} class={cn("min-w-0!", props.contentClass)}>
-          {props.children}
-        </div>
-      </div>
-      <Show when={api().hasOverflowY}>
-        <div
-          {...api().getScrollbarProps({ orientation: "vertical" })}
-          class="absolute right-0 top-0 flex h-full w-2.5 touch-none p-px transition-colors select-none"
-        >
-          <div
-            {...api().getThumbProps({ orientation: "vertical" })}
-            class="bg-border relative w-full rounded-full"
-          />
-        </div>
-      </Show>
-      <Show when={api().hasOverflowX}>
-        <div
-          {...api().getScrollbarProps({ orientation: "horizontal" })}
-          class="absolute bottom-0 left-0 flex h-2.5 w-full touch-none p-px transition-colors select-none"
-        >
-          <div
-            {...api().getThumbProps({ orientation: "horizontal" })}
-            class="bg-border relative rounded-full"
-          />
-        </div>
-      </Show>
-    </div>
+    <ScrollAreaPrimitive.Root class={props.rootClass}>
+      <ScrollAreaPrimitive.Viewport class={props.viewportClass}>
+        {props.children}
+      </ScrollAreaPrimitive.Viewport>
+      <ScrollAreaPrimitive.Scrollbar class="m-1 flex w-1 justify-center rounded bg-transparent opacity-0 transition-opacity pointer-events-none data-[hovering]:opacity-100 data-[hovering]:delay-0 data-[hovering]:pointer-events-auto data-[scrolling]:opacity-100 data-[scrolling]:duration-0 data-[scrolling]:pointer-events-auto">
+        <ScrollAreaPrimitive.Thumb class="w-full rounded bg-black/20 dark:bg-white/20" />
+      </ScrollAreaPrimitive.Scrollbar>
+    </ScrollAreaPrimitive.Root>
   );
 }
-
-/**
- * <ScrollAreaPrimitive.ScrollAreaScrollbar
-      data-slot="scroll-area-scrollbar"
-      orientation={orientation}
-      className={cn(
-        "flex touch-none p-px transition-colors select-none",
-        orientation === "vertical" &&
-          "h-full w-2.5 border-l border-l-transparent",
-        orientation === "horizontal" &&
-          "h-2.5 flex-col border-t border-t-transparent",
-        className
-      )}
-      {...props}
-    >
-      <ScrollAreaPrimitive.ScrollAreaThumb
-        data-slot="scroll-area-thumb"
-        className="bg-border relative flex-1 rounded-full"
-      />
-    </ScrollAreaPrimitive.ScrollAreaScrollbar>
- */
