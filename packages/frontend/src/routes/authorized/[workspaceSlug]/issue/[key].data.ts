48b188323f5e0f48a7502bda6bf918e9b629b444
fdemb <69842744+fdemb@users.noreply.github.com>
2026-02-13 00:18:00 +0100

    perf: use fully parallel loading by extracting the team key from url

--- packages/frontend/src/routes/authorized/[workspaceSlug]/issue/[key].data.ts
+++ packages/frontend/src/routes/authorized/[workspaceSlug]/issue/[key].data.ts
@@ -2,14 +2,15 @@ import { api } from "@/lib/api";
 import { query } from "@solidjs/router";
 
 export const issueLoader = query(async (issueKey: string, workspaceSlug: string) => {
-  const issueRes = await api.api.issues[":issueKey"].$get({
-    param: { issueKey },
-  });
+  const teamKey = issueKey.split("-")[0];
+  if (!teamKey) {
+    throw new Error("Invalid issue key");
+  }
 
-  const { issue } = await issueRes.json();
-  const teamKey = issue.team?.key;
-
-  const [labelsRes, membersRes, sprintsRes] = await Promise.all([
+  const [issueRes, labelsRes, membersRes, sprintsRes] = await Promise.all([
+    api.api.issues[":issueKey"].$get({
+      param: { issueKey },
+    }),
     api.api.labels.$get(),
     api.api.workspaces[":slug"].members.$get({
       param: { slug: workspaceSlug },
@@ -21,6 +22,7 @@ export const issueLoader = query(async (issueKey: string, workspaceSlug: string)
       : null,
   ]);
 
+  const { issue } = await issueRes.json();
   const { labels } = await labelsRes.json();
   const { members } = await membersRes.json();
   const sprints = sprintsRes ? (await sprintsRes.json()).sprints : [];
